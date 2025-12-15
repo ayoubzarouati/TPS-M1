563d03b32a223968de8c1e453c0c3681aebd5133
ayoub zarouati <zerouati.ayoub@gmail.com>
2025-12-15 22:46:29 +0100

    ANGULAR

--- Architecture JEE et Middlewares/Activité Pratique N°3 - Angular Framework/README.md
+++
@@ -1,8 +0,0 @@
-# Angular Framework frontend & backend
-
-## this is the project presented using screens from the execution
-![](imgs/1.png)
-![](imgs/2.png)
-![](imgs/3.png)
-![](imgs/4.png)
-![](imgs/5.png)
--- Architecture JEE et Middlewares/Activité Pratique N°3 - Angular Framework/products_service/src/main/java/com/vogdo/products_service/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vogdo.products_service.repository;
-
-import com.vogdo.products_service.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
